b36c072b882e2e892e430fec492e34f8bf188591
Gavin Allen <goonerlagoon@gmail.com>
2022-02-24 01:31:11 -0500

    completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let tempStr = ''
+  if (num < 0) return 'ERROR';
+  for (let i = 0; i < num; i++) {
+    tempStr += string;
+  }
+  return tempStr;
 };
 
 // Do not edit below this line
